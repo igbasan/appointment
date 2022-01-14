679919aa33ea36570796b4c14aa3d75d4f08831a
John Henry <john04star@gmail.com>
2022-01-14 14:27:53 +0100

    completed route and redux setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Index from "./component/landing/Index";
+import Login from "./component/auth/Login";
+import Register from "./component/auth/Register";
 
-const App =()=> <Index/>
+const App =()=> {
+    return(
+        <Router>
+            <Routes>
+                <Route path="/" element={<Index/>}></Route>
+                <Route path="/login" element={<Login/>}></Route>
+                <Route path="/register" element={<Register/>}></Route>
+            </Routes>
+           
+        </Router>
+    );
+}
 
 export default App;
